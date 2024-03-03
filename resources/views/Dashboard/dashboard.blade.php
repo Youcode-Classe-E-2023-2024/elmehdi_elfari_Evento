c2a6544b7c2c609b1cafaaf941141dbe327a8e4d
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-03-03 21:52:04 +0100

    page dashboard

--- resources/views/Dashboard/dashboard.blade.php
+++ resources/views/Dashboard/dashboard.blade.php
@@ -1342,52 +1342,7 @@
     </style>
 </head>
 <body class="text-gray-800 font-inter">
-<!--sidenav -->
-<div class="fixed left-0 top-0 w-64 h-full bg-[#f8f4f3] p-4 z-50 sidebar-menu transition-transform">
-    <a href="#" class="flex items-center pb-4 border-b border-b-gray-800">
-
-        <h2 class="font-bold text-2xl">LOREM <span class="bg-[#f84525] text-white px-2 rounded-md">Evento</span></h2>
-    </a>
-    <ul class="mt-4">
-        <span class="text-gray-400 font-bold">ADMIN</span>
-        <li class="mb-1 group">
-            <a href="" class="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                <i class="ri-home-2-line mr-3 text-lg"></i>
-                <span class="text-sm">Dashboard</span>
-            </a>
-        </li>
-        <li class="mb-1 group">
-            <a href="" class="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100 sidebar-dropdown-toggle">
-                <i class='bx bx-user mr-3 text-lg'></i>
-                <span class="text-sm">Users</span>
-                <i class="ri-arrow-right-s-line ml-auto group-[.selected]:rotate-90"></i>
-            </a>
-            <ul class="pl-7 mt-2 hidden group-[.selected]:block">
-                <li class="mb-4">
-                    <a href="" class="text-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">All</a>
-                </li>
-                <li class="mb-4">
-                    <a href="" class="text-gray-900 text-sm flex items-center hover:text-[#f84525] before:contents-[''] before:w-1 before:h-1 before:rounded-full before:bg-gray-300 before:mr-3">Roles</a>
-                </li>
-            </ul>
-        </li>
-        <li class="mb-1 group">
-            <a href="{{ route('get.event') }}" class="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                <i class='bx bx-list-ul mr-3 text-lg'></i>
-                <span class="text-sm">Event</span>
-            </a>
-        </li>
-        <li class="mb-1 group">
-            <a href="{{ route('get.category') }}" class="flex font-semibold items-center py-2 px-4 text-gray-900 hover:bg-gray-950 hover:text-gray-100 rounded-md group-[.active]:bg-gray-800 group-[.active]:text-white group-[.selected]:bg-gray-950 group-[.selected]:text-gray-100">
-                <i class='bx bx-list-ul mr-3 text-lg'></i>
-                <span class="text-sm">Category</span>
-            </a>
-        </li>
-    </ul>
-</div>
-<div class="fixed top-0 left-0 w-full h-full bg-black/50 z-40 md:hidden sidebar-overlay"></div>
-<!-- end sidenav -->
-
+@include('layouts.sedbar')
 <main class="w-full md:w-[calc(100%-256px)] md:ml-64 bg-gray-200 min-h-screen transition-all main">
     <!-- navbar -->
     <div class="py-2 px-6 bg-[#f8f4f3] flex items-center shadow-md shadow-black/5 sticky top-0 left-0 z-30">
@@ -1395,170 +1350,16 @@
             <i class="ri-menu-line"></i>
         </button>
 
-        <ul class="ml-auto flex items-center">Blo
-            <li class="mr-1 dropdown">
-                <div class="dropdown-menu shadow-md shadow-black/5 z-30 hidden max-w-xs w-full bg-white rounded-md border border-gray-100">
-                    <form action="" class="p-4 border-b border-b-gray-100">
-                        <div class="relative w-full">
-                            <input type="text" class="py-2 pr-4 pl-10 bg-gray-50 w-full outline-none border border-gray-100 rounded-md text-sm focus:border-blue-500" placeholder="Search...">
-                            <i class="ri-search-line absolute top-1/2 left-4 -translate-y-1/2 text-gray-900"></i>
-                        </div>
-                    </form>
-                </div>
-            </li>
-            <li class="dropdown">
-                <div class="dropdown-menu shadow-md shadow-black/5 z-30 hidden max-w-xs w-full bg-white rounded-md border border-gray-100">
-                    <div class="flex items-center px-4 pt-4 border-b border-b-gray-100 notification-tab">
-                        <button type="button" data-tab="notification" data-tab-page="notifications" class="text-gray-400 font-medium text-[13px] hover:text-gray-600 border-b-2 border-b-transparent mr-4 pb-1 active">Notifications</button>
-                        <button type="button" data-tab="notification" data-tab-page="messages" class="text-gray-400 font-medium text-[13px] hover:text-gray-600 border-b-2 border-b-transparent mr-4 pb-1">Messages</button>
-                    </div>
-                    <div class="my-2">
-                        <ul class="max-h-64 overflow-y-auto" data-tab-for="notification" data-page="notifications">
-                            <li>
-                                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                                    <div class="ml-2">
-                                        <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">New order</div>
-                                        <div class="text-[11px] text-gray-400">from a user</div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                                    <div class="ml-2">
-                                        <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">New order</div>
-                                        <div class="text-[11px] text-gray-400">from a user</div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                                    <div class="ml-2">
-                                        <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">New order</div>
-                                        <div class="text-[11px] text-gray-400">from a user</div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                                    <div class="ml-2">
-                                        <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">New order</div>
-                                        <div class="text-[11px] text-gray-400">from a user</div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                                    <div class="ml-2">
-                                        <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">New order</div>
-                                        <div class="text-[11px] text-gray-400">from a user</div>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                        <ul class="max-h-64 overflow-y-auto hidden" data-tab-for="notification" data-page="messages">
-                            <li>
-                                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                                    <div class="ml-2">
-                                        <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">John Doe</div>
-                                        <div class="text-[11px] text-gray-400">Hello there!</div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                                    <div class="ml-2">
-                                        <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">John Doe</div>
-                                        <div class="text-[11px] text-gray-400">Hello there!</div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                                    <div class="ml-2">
-                                        <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">John Doe</div>
-                                        <div class="text-[11px] text-gray-400">Hello there!</div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                                    <div class="ml-2">
-                                        <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">John Doe</div>
-                                        <div class="text-[11px] text-gray-400">Hello there!</div>
-                                    </div>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#" class="py-2 px-4 flex items-center hover:bg-gray-50 group">
-                                    <img src="https://placehold.co/32x32" alt="" class="w-8 h-8 rounded block object-cover align-middle">
-                                    <div class="ml-2">
-                                        <div class="text-[13px] text-gray-600 font-medium truncate group-hover:text-blue-500">John Doe</div>
-                                        <div class="text-[11px] text-gray-400">Hello there!</div>
-                                    </div>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </li>
-            <script>
-                const fullscreenButton = document.getElementById('fullscreen-button');
-
-                fullscreenButton.addEventListener('click', toggleFullscreen);
-
-                function toggleFullscreen() {
-                    if (document.fullscreenElement) {
-                        // If already in fullscreen, exit fullscreen
-                        document.exitFullscreen();
-                    } else {
-                        // If not in fullscreen, request fullscreen
-                        document.documentElement.requestFullscreen();
-                    }
-                }
-            </script>
-
-            <li class="dropdown ml-3">
-                <button type="button" class="dropdown-toggle flex items-center">
-                    <div class="flex-shrink-0 w-10 h-10 relative">
-                        <div class="p-1 bg-white rounded-full focus:outline-none focus:ring">
-                            <img class="w-8 h-8 rounded-full" src="https://laravelui.spruko.com/tailwind/ynex/build/assets/images/faces/9.jpg" alt=""/>
-                            <div class="top-0 left-7 absolute w-3 h-3 bg-lime-400 border-2 border-white rounded-full animate-ping"></div>
-                            <div class="top-0 left-7 absolute w-3 h-3 bg-lime-500 border-2 border-white rounded-full"></div>
-                        </div>
-                    </div>
-                    <div class="p-2 md:block text-left">
-                        <h2 class="text-sm font-semibold text-gray-800">John Doe</h2>
-                        <p class="text-xs text-gray-500">Administrator</p>
-                    </div>
+        <button type="button" class="dropdown-toggle flex items-center">
+            <form method="POST" action="{{ url('/logout') }}">
+                @csrf
+                <button type="submit" class="dropdown-toggle flex items-center">
+                    <a role="menuitem" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-[#f84525] hover:bg-gray-50 cursor-pointer">
+                        Log Out
+                    </a>
                 </button>
-                <ul class="dropdown-menu shadow-md shadow-black/5 z-30 hidden py-1.5 rounded-md bg-white border border-gray-100 w-full max-w-[140px]">
-                    <li>
-                        <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-[#f84525] hover:bg-gray-50">Profile</a>
-                    </li>
-                    <li>
-                        <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-[#f84525] hover:bg-gray-50">Settings</a>
-                    </li>
-                    <li>
-                        <form method="POST" action="">
-                            <a role="menuitem" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-[#f84525] hover:bg-gray-50 cursor-pointer"
-                               onclick="event.preventDefault();
-                                    this.closest('form').submit();">
-                                Log Out
-                            </a>
-                        </form>
-                    </li>
-                </ul>
-            </li>
-        </ul>
+            </form>
+        </button>
     </div>
     <!-- end navbar -->
 
@@ -1569,9 +1370,9 @@
                 <div class="flex justify-between mb-6">
                     <div>
                         <div class="flex items-center mb-1">
-                            <div class="text-2xl font-semibold">2</div>
+                            <div class="text-2xl font-semibold">{{ $categoriesCount }}</div>
                         </div>
-                        <div class="text-sm font-medium text-gray-400">Users</div>
+                        <div class="text-sm font-medium text-gray-400">Category</div>
                     </div>
                     <div class="dropdown">
                         <button type="button" class="dropdown-toggle text-gray-400 hover:text-gray-600"><i class="ri-more-fill"></i></button>
@@ -1601,12 +1402,6 @@
                     <div class="dropdown">
                         <button type="button" class="dropdown-toggle text-gray-400 hover:text-gray-600"><i class="ri-more-fill"></i></button>
                         <ul class="dropdown-menu shadow-md shadow-black/5 z-30 hidden py-1.5 rounded-md bg-white border border-gray-100 w-full max-w-[140px]">
-                            <li>
-                                <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Profile</a>
-                            </li>
-                            <li>
-                                <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Settings</a>
-                            </li>
                             <li>
                                 <a href="#" class="flex items-center text-[13px] py-1.5 px-4 text-gray-600 hover:text-blue-500 hover:bg-gray-50">Logout</a>
                             </li>
