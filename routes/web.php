aba4a9eea17f89fc5e8c18b46a15d0ad4ddd5ee4
ELMEHDI_ELFARI <144785903+ElmehdiElfari@users.noreply.github.com>
2024-03-03 11:17:55 +0100

    Créé les route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ForgotPasswordLinkController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +24,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard',[DashboardController::class,'show'])->name('dash');
+
+/* Auth*/
+Route::get("register", [RegisterController::class, 'create'])->name('Form-register');
+
+Route::post("register", [RegisterController::class, 'store'])->name('register');
+
+Route::post('logout', [LogoutController::class, 'destroy']);
+
+Route::get("login", [LoginController::class, 'create'])->name('Form-login');
+
+Route::post("login", [LoginController::class, 'authenticate'])->name('login');
+
+
+Route::get('/request', [ForgotPasswordLinkController::class, 'create']);
+
+Route::post('/request', [ForgotPasswordLinkController::class, 'store']);
+
+Route::get('password/reset/{token}', [ForgotPasswordController::class, 'create'])->name('password.reset');
+
+Route::post('/reset', [ForgotPasswordController::class, 'reset'])->name('reset');
+
+Route::get("category", [CategoryController::class, 'index'])->name('get.category');
+
+Route::get("Event", [EventController::class, 'index'])->name('get.event');
